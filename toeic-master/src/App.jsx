a23a1973eb97450cd97afbd5da153a51b46d702f
lhnam11 <nvhoainam126@gmail.com>
2024-11-24 15:29:25 +0700

    Add adminPage in pages

--- toeic-master/src/App.jsx
+++ toeic-master/src/App.jsx
@@ -1,22 +1,35 @@
-import { useState } from 'react';
-
-import './App.css';
-
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-
 import Home from './pages/HomePage/Home';
 import ListeningPage from './pages/listeningPages/ListeningPage';
 import ReadingPage from './pages/readingPage/ReadingPage';
+import Topics from './pages/adminPage/topics';
+import AdminHome from './pages/adminPage/adminHome'; 
+import VocabularyA from './pages/adminPage/vocabularyA'; 
+import LessonA from './pages/adminPage/lessonA'; 
+import ExamsA from './pages/adminPage/examsA';
+
 function App() {
   return (
     <Router>
-    <Routes><Route path="/" element={<Home/>} /></Routes>
-    <Routes><Route path="/listening" element={<ListeningPage/>} /></Routes>
-    <Routes><Route path="/reading" element={<ReadingPage/>} /></Routes>
-    <Routes><Route path="/test" element={<Home/>} /></Routes>
-    <Routes><Route path="/login" element={<Home/>} /></Routes>
-    <Routes><Route path="/lessons" element={<Home/>} /></Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/listening" element={<ListeningPage />} />
+        <Route path="/reading" element={<ReadingPage />} />
+        <Route path="/test" element={<Home />} />
+        <Route path="/lessons" element={<Home />} />
+        <Route path="/login" element={<AdminHome />} />  {/* AdminHome cho login */}
+        
+        {/* Route cha cho Admin */}
+        <Route path="/admin" element={<AdminHome />}>
+          <Route path="topics" element={<Topics />} /> {/* Route cho Topics */}
+          <Route path="vocabulary" element={<VocabularyA />} /> {/* Route cho Vocabulary */}
+          <Route path="lesson" element={<LessonA />} /> {/* Route cho Lesson */}
+          <Route path="exams" element={<ExamsA />} /> {/* Route cho Exams */}
+        </Route>
+
+        {/* Các route khác có thể thêm vào đây */}
+      </Routes>
+    </Router>
   );
 }
 
