a01a2d9241da9cb40b705120d10593bdddb3216a
Trung Kien <htrungkien1806@gmail.com>
2024-11-25 21:36:52 +0700

    chỉnh lại code lỗi

--- toeic-master/src/App.jsx
+++ toeic-master/src/App.jsx
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
+
 import { useState } from 'react';
 import './App.css';
-=======
->>>>>>> 75fac0d313fdddcb6eba0a6d8193006b8907defa
+
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/HomePage/Home';
 
@@ -10,54 +9,38 @@ import ListeningPage from './pages/listeningPages/ListeningPage';
 import Part from "./pages/listeningPages/Part"
 
 import ReadingPage from './pages/readingPage/ReadingPage';
-<<<<<<< HEAD
+
 
 import LessonList from './pages/lessonsPages/LessonList';
 import LessonDetail from "./pages/lessonsPages/LessonDetail"
 
 
-=======
+
 import Topics from './pages/adminPage/topics';
-import AdminHome from './pages/adminPage/adminHome'; 
-import VocabularyA from './pages/adminPage/vocabularyA'; 
-import LessonA from './pages/adminPage/lessonA'; 
+import AdminHome from './pages/adminPage/adminHome';
+import VocabularyA from './pages/adminPage/vocabularyA';
+import LessonA from './pages/adminPage/lessonA';
 import ExamsA from './pages/adminPage/examsA';
 import QuestionPart from './pages/readingPage/QuestionPart';
-<<<<<<< HEAD
-import LessonList from './pages/lessonsPages/LessonList';
+
 import Reading from './pages/lessonsPages/Reading';
 
-=======
->>>>>>> 75fac0d313fdddcb6eba0a6d8193006b8907defa
->>>>>>> f1bd7dccb275353191fdd981d994f21aa182e38d
 
 
 function App() {
   return (
     <Router>
-<<<<<<< HEAD
-      <Routes><Route path="/" element={<Home />} /></Routes>
-      <Routes>
-        <Route path="/listening" element={<ListeningPage />} />
-        <Route path="/listening/:part" element={<Part />} />
-      </Routes>
-      <Routes><Route path="/reading" element={<ReadingPage />} /></Routes>
-      <Routes><Route path="/test" element={<Home />} /></Routes>
-      <Routes><Route path="/login" element={<Home />} /></Routes>
-      <Routes>
-        <Route path="/lessons" element={<LessonList />} />
-        <Route path="/lessons/:id" element={<LessonDetail />} />
-=======
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/listening" element={<ListeningPage />} />
-        <Route path="/reading" element={<ReadingPage/>} />
+        <Route path="/reading" element={<ReadingPage />} />
         <Route path="/reading/part/:part" element={<QuestionPart />} />
         <Route path="/test" element={<Home />} />
         <Route path="/lessons" element={<LessonList />} />
-        <Route path="/Reading" element={<Reading />} /> {/* bs thử kh chạy link  */} 
+        <Route path="/lessons/:id" element={<LessonDetail />} />
+        <Route path="/Reading" element={<Reading />} /> {/* bs thử kh chạy link  */}
         <Route path="/login" element={<AdminHome />} />  {/* AdminHome cho login */}
-        
+
         {/* Route cha cho Admin */}
         <Route path="/admin" element={<AdminHome />}>
           <Route path="topics" element={<Topics />} /> {/* Route cho Topics */}
@@ -67,7 +50,6 @@ function App() {
         </Route>
 
         {/* Các route khác có thể thêm vào đây */}
->>>>>>> 75fac0d313fdddcb6eba0a6d8193006b8907defa
       </Routes>
     </Router>
   );
